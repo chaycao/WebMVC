c51e4330992635fba8a4450bfd46cec1729d952b
chaycao <429629038@qq.com>
2018-05-30 16:16:35 +0800

    添加带默认值的方法

--- src/main/java/com/chaycao/webmvc/context/PropertiesContext.java
+++ src/main/java/com/chaycao/webmvc/context/PropertiesContext.java
@@ -28,13 +28,26 @@ public class PropertiesContext {
     }
 
     /**
-     * 获得配置文件中的配置
+     * 获得配置
      * @param name
      * @return 配置值，若为name为null或空字符串，返回null
      */
-    public static String getProperty(String name, String defaultValue) {
+    public static String getProperty(String name) {
         if (name == null || name.equals(""))
             return null;
         return getInstance().prop.getProperty(name);
     }
+
+    /**
+     * 带默认值的获取配置
+     * @param name 配置名
+     * @param defaultValue 默认值
+     * @return 配置值，若为name为null或空字符串，返回null；若name不存在，返回默认值
+     */
+    public static String getProperty(String name, String defaultValue) {
+        if (name == null || name.equals(""))
+            return null;
+        return getInstance().prop.getProperty(name, defaultValue);
+    }
+
 }
