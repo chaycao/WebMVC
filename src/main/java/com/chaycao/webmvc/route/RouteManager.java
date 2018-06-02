435fd1de61e50d5cd5838d4c4cf78f7478b26bd0
chaycao <429629038@qq.com>
2018-06-02 20:40:13 +0800

    route是否支持request的逻辑移出到Route.java

--- src/main/java/com/chaycao/webmvc/route/RouteManager.java
+++ src/main/java/com/chaycao/webmvc/route/RouteManager.java
@@ -1,8 +1,8 @@
 package com.chaycao.webmvc.route;
 
 import com.chaycao.webmvc.annotation.RequestMapping;
+import com.chaycao.webmvc.annotation.RequestMethod;
 import com.chaycao.webmvc.util.PathUtil;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -41,11 +41,13 @@ public class RouteManager {
         for (Class<?> controller : controllerManager.getControllers()) {
             String basePath = getRequestMappingValue(controller);
             for (Method m : controller.getDeclaredMethods()) {
-                String routPath = getRequestMappingValue(m);
-                if (routPath == null)
-                    continue;
-                String path = basePath + routPath;
-                routes.add(new Route(path, m));
+                if (isRequestMapping(m)) {
+                    String path = basePath + getRequestMappingValue(m);
+                    RequestMethod[] methods = getRequestMappingMethods(m);
+                    String[] produces = getRequestMappingProduces(m);
+                    String[] consumes = getRequestMappingConsumes(m);
+                    routes.add(new Route(path, m, methods, produces, consumes));
+                }
             }
         }
         return routes;
@@ -57,23 +59,30 @@ public class RouteManager {
      * @return 符合的Rounte对象，找不到返回null
      */
     public Route findRouteByRequest(HttpServletRequest request) {
-        String path = PathUtil.getRelativePath(request);
-        Route route = findRouteByPath(path);
-        return route;
+        List<Route> routes = findRoutesByPath(this.getRoutes(), request);
+        for (Route route : routes) {
+            if (route.canSupportRequest(request))
+                return route;
+        }
+        return null;
     }
 
     /**
      *
-     * @param path
+     *
+     * @param list
+     * @param request
      * @return 符合的Route对象，若没找到返回null
      */
-    public Route findRouteByPath(String path) {
-        for (Route route : this.routes) {
+    public List<Route> findRoutesByPath(List<Route> list, HttpServletRequest request) {
+        String path = PathUtil.getRelativePath(request);
+        List<Route> res = new ArrayList<>();
+        for (Route route : list) {
             if (route.getPath().equals(path)) {
-                return route;
+                res.add(route);
             }
         }
-        return null;
+        return res;
     }
 
     /**
@@ -91,7 +100,7 @@ public class RouteManager {
     /**
      *
      * @param method
-     * @return 获得RequestMapping的值，若没有则返回null
+     * @return 获得RequestMapping的Value，若没有则返回null
      */
     private String getRequestMappingValue(Method method) {
         RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
@@ -104,6 +113,48 @@ public class RouteManager {
         if (method.getAnnotation(RequestMapping.class) == null)
             return false;
         return true;
+    }
+
+    private RequestMethod[] getRequestMappingMethods(Class<?> clazz) {
+        RequestMapping requestMapping = clazz.getAnnotation(RequestMapping.class);
+        if (requestMapping != null)
+            return requestMapping.method();
+        return null;
+    }
+
+    private RequestMethod[] getRequestMappingMethods(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping != null)
+            return requestMapping.method();
+        return null;
+    }
+
+    private String[] getRequestMappingProduces(Class<?> clazz) {
+        RequestMapping requestMapping = clazz.getAnnotation(RequestMapping.class);
+        if (requestMapping != null)
+            return requestMapping.produces();
+        return null;
+    }
+
+    private String[] getRequestMappingProduces(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping != null)
+            return requestMapping.produces();
+        return null;
+    }
+
+    private String[] getRequestMappingConsumes(Class<?> clazz) {
+        RequestMapping requestMapping = clazz.getAnnotation(RequestMapping.class);
+        if (requestMapping != null)
+            return requestMapping.consumes();
+        return null;
+    }
+
+    private String[] getRequestMappingConsumes(Method method) {
+        RequestMapping requestMapping = method.getAnnotation(RequestMapping.class);
+        if (requestMapping != null)
+            return requestMapping.consumes();
+        return null;
     }
 
     public void addRoute(Route r) {
